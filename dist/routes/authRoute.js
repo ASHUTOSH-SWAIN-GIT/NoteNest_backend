00a98087a2029ba15c1444107cca0259569d4357
ASHUTOSH-SWAIN-GIT <ashutoshswain7383@gmail.com>
2025-04-27 01:16:21 +0530

    added start script

--- dist/routes/authRoute.js
+++ dist/routes/authRoute.js
@@ -2,10 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 // routes/notesRoute.ts
 const express_1 = require("express");
-const authController_1 = require("../controller/authController");
+// import { googleLogin } from "../controller/authController"
 const router = (0, express_1.Router)();
 // Define your routes
-router.post('/signup', authController_1.SignUp);
+// router.post('/signup',googleLogin );
 router.get(`/login`);
 // This is important
 exports.default = router;
